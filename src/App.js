e5906643be75687dbf285edbb5aa8db960b1d146
vishnu sharma <vishnuatdaftar@gmail.com>
2024-01-11 05:52:57 +0530

    lists

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
 import "./App.css";
 import { SplitScreen } from "./components/split-screen";
+import { LargeAuthorListItem } from "./components/authors/LargeListItems";
+import { SmallAuthorListItem } from "./components/authors/SmallListItems";
+import { LargeBookListItem } from "./components/books/LargeListItems";
+import { SmallBookListItem } from "./components/books/SmallListItems";
+import { NumberedList } from "./components/lists/Numbered";
+import { RegularList } from "./components/lists/Regular";
+import { authors } from "./data/authors";
+import { books } from "./data/books";
 
-const LeftSide = ({ title }) => {
+const LeftSideComp = ({ title }) => {
   return <h2 style={{ backgroundColor: "crimson" }}>{title}</h2>;
 };
 
-const RightSide = ({ title }) => {
+const RightSideComp = ({ title }) => {
   return <h2 style={{ backgroundColor: "burlywood" }}>{title}</h2>;
 };
 
 function App() {
   return (
-    <SplitScreen leftWidth={1} rightWidth={3}>
-      <LeftSide title={"Right"} />
-      <RightSide title={"Left"} />
-    </SplitScreen>
+    // <SplitScreen leftWidth={1} rightWidth={3}>
+    //   <LeftSideComp title={"Right"} />
+    //   <RightSideComp title={"Left"} />
+    // </SplitScreen>
+    <>
+      <RegularList
+        items={authors}
+        sourceName={"author"}
+        ItemComponent={SmallAuthorListItem}
+      />
+      <NumberedList
+        items={authors}
+        sourceName={"author"}
+        ItemComponent={LargeAuthorListItem}
+      />
+
+      <RegularList
+        items={books}
+        sourceName={"book"}
+        ItemComponent={SmallBookListItem}
+      />
+
+      <NumberedList
+        items={books}
+        sourceName={"book"}
+        ItemComponent={LargeBookListItem}
+      />
+    </>
   );
 }
 
